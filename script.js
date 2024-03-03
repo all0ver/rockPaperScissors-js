62a688469287af690eb1c0e97069e1bb770013c5
Mateusz Karpiński <mateusz.karpinski29@gmail.com>
2024-03-04 00:17:33 +0100

    finished game

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)];
 }
 
-function round(computerSelection) {
-    let playerSelection = prompt("Choose your element: ").toLowerCase();
+function round(playerSelection, computerSelection) {
     let score = playerSelection+computerSelection;
     console.log(score);
 
@@ -23,37 +22,72 @@ function round(computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let result = 0;
-    for (let i = 0; i < 5; i++) {
-            result = parseInt(round(getComputerChoice()));
-            switch (result) {
-                case 0:
-                    playerScore += 1;
-                    computerScore += 1;
-                    break;
-                case 1:
-                    playerScore += 1;
-                    break;
-                case 2:
-                    computerScore += 1;
-                    break;
-                case 3:
-                    i = i-1;
-                    break;
-                default:
-                    break;
-            }
-        }
-    if (playerScore > computerScore) {
-        console.log("You have won the game! The score: " + playerScore + ":" + computerScore);
-    } else if (playerScore < computerScore) {
-        console.log("You have lost the game! The score: " + playerScore + ":" + computerScore);
-    } else {
-        console.log("The game is tied! The score: " + playerScore + ":" + computerScore);
-    }
-}
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let result = 0;
+//     while (playerScore != 3 || computerScore != 3) {
+//             switch (result) {
+//                 case 0:
+//                     playerScore += 1;
+//                     computerScore += 1;
+//                     break;
+//                 case 1:
+//                     playerScore += 1;
+//                     break;
+//                 case 2:
+//                     computerScore += 1;
+//                     break;
+//                 default:
+//                     break;
+//             }
+//             document.querySelector('.show-score-p').textContent = playerScore + ":" + computerScore;
+//         }
+//     // if (playerScore > computerScore) {
+//     //     console.log("You have won the game! The score: " + playerScore + ":" + computerScore);
+//     // } else if (playerScore < computerScore) {
+//     //     console.log("You have lost the game! The score: " + playerScore + ":" + computerScore);
+//     // } else {
+//     //     console.log("The game is tied! The score: " + playerScore + ":" + computerScore);
+//     }
+// }
 
-game();
+
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+                let computerChoice = getComputerChoice();
+                switch (round(button.id, computerChoice)) {
+                    case 0:
+                        playerScore += 0;
+                        computerScore += 0;
+                        break;
+                    case 1:
+                        playerScore += 1;
+                        break;
+                    case 2:
+                        computerScore += 1;
+                        break;
+                    default:
+                        break;
+                }
+                document.querySelector('.score-round').textContent = button.id + " vs " + computerChoice;
+                
+                if (playerScore == 5) {
+                    document.querySelector('.show-score-p').textContent = 'You won!';
+                    playerScore = 0;
+                    computerScore = 0;
+                } else if(computerScore == 5) {
+                    document.querySelector('.show-score-p').textContent = 'You lost!';
+                        playerScore = 0;
+                        computerScore = 0;
+                } else {
+                    document.querySelector('.show-score-p').textContent = playerScore + ":" + computerScore;
+                }
+            })
+});
+
+
